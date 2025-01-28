a5d68523a9bb5cd8d04eea1ca7c567fc2d41019a
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2025-01-28 08:22:24 +0500

    Finishing header and Implementing Apis

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { ReactQueryProvider } from "@/providers/tanstack/react-query-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
+        <ReactQueryProvider>
+          <Header />
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
